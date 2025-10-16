70cefa85b1135538592c6e06630e124cbb4b5448
DONTKILLME <ashishnegi0511@gmail.com>
2025-10-16 21:23:30 +0530

    Required Success Message format added

--- utils/responses.ts
+++ utils/responses.ts
@@ -5,3 +5,12 @@ export function errorMessage(error: string, message: string) {
     message,
   };
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function successMessage(message: string, data?: any) {
+  return {
+    isSuccessful: true,
+    message,
+    data: data || null,
+  };
+}
