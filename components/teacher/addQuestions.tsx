e05b28ace0864ceacf08097c999100ebe98fa4d3
DONTKILLME <ashishnegi0511@gmail.com>
2025-10-18 13:16:08 +0530

    Planned UI for adding Questions.

---
+++ components/teacher/addQuestions.tsx
@@ -0,0 +1,179 @@
+import React, { Dispatch, SetStateAction } from "react";
+import { PhotoIcon } from "@heroicons/react/24/solid";
+
+// import { TeacherDocument } from "@/structures/interfaceFile";
+
+// interface AddTestsProps {
+//   teacherData: TeacherDocument;
+//   teacherCode: string;
+//   setTeacherData: Dispatch<SetStateAction<TeacherDocument | null>>;
+// }
+
+export function AddTests() {
+  //   const handleDeleteTest = async (test_code: string) => {
+  //     const payload: DeleteTeacherPayload = {
+  //       teacher: {
+  //         code: teacherCode,
+  //         test_code: test_code,
+  //       },
+  //     };
+
+  //     try {
+  //       const new_tests = await delTeacher(payload);
+  //       console.log("Deleted successfully:", new_tests);
+  //       if (!new_tests.isSuccessful) {
+  //         throw new Error(new_tests.message);
+  //       }
+  //       setTeacherData(new_tests.data);
+  //     } catch (err) {
+  //       console.error("Error deleting teacher:", err);
+  //     }
+  //   };
+  return (
+    <div
+      className="max-w-6xl mx-auto min-h-fit
+"
+    >
+      <div className="flex flex-wrap gap-4 max-h-[80vh]  p-4 justify-center">
+        <form>
+          <div className="space-y-12">
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="col-span-full">
+                <label
+                  htmlFor="cover-photo"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Cover photo
+                </label>
+                <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                  <div className="text-center">
+                    <PhotoIcon
+                      aria-hidden="true"
+                      className="mx-auto size-12 text-gray-300"
+                    />
+                    <div className="mt-4 flex text-sm/6 text-gray-600">
+                      <label
+                        htmlFor="file-upload"
+                        className="relative cursor-pointer rounded-md bg-transparent font-semibold text-indigo-600 focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-indigo-600 hover:text-indigo-500"
+                      >
+                        <span>Upload a file</span>
+                        <input
+                          id="file-upload"
+                          name="file-upload"
+                          type="file"
+                          className="sr-only"
+                        />
+                      </label>
+                      <p className="pl-1">or drag and drop</p>
+                    </div>
+                    <p className="text-xs/5 text-gray-600">
+                      PNG, JPG, GIF up to 10MB
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base/7 font-semibold text-gray-900">
+              Personal Information
+            </h2>
+            <p className="mt-1 text-sm/6 text-gray-600">
+              Use a permanent address where you can receive mail.
+            </p>
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  First name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="first-name"
+                    name="first-name"
+                    type="text"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="last-name"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Last name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="last-name"
+                    name="last-name"
+                    type="text"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="email"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+
+              <div className="col-span-full">
+                <label
+                  htmlFor="street-address"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Street address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="street-address"
+                    name="street-address"
+                    type="text"
+                    autoComplete="street-address"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button
+              type="button"
+              className="text-sm/6 font-semibold text-gray-900"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
